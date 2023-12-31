6f18638a8a263d7543f0894782173577940e915c
klazapp <klazapp@klazapp.com>
2023-12-31 17:19:06 +0800

    Add files via upload

---
+++ Runtime/Logger/System/Logger.cs
@@ -0,0 +1,40 @@
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
+
+namespace com.Klazapp.Utility
+{
+    //Logger allows for a simple on/off switch to enable/disable all logs in app
+    //Instead of manually commenting/adding them in each individual scripts
+    public static class LogMessage 
+    {
+        [SuppressMessage("ReSharper", "Unity.PerformanceCriticalCodeInvocation")]
+        [Conditional("ENABLE_LOGS")]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void Debug(string message)
+        {
+            //ReSharper disable once Unity.PerformanceCriticalCodeInvocation
+            UnityEngine.Debug.Log("<color=#82f3ff>" + message + "</color>");
+        }
+        
+        [SuppressMessage("ReSharper", "Unity.PerformanceCriticalCodeInvocation")]
+        [Conditional("ENABLE_LOGS")]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void DebugError(string message)
+        {
+            //ReSharper disable once Unity.PerformanceCriticalCodeInvocation
+            UnityEngine.Debug.LogError("<color=red>" + message + "</color>");
+            //ReSharper disable once Unity.PerformanceCriticalCodeInvocation
+            UnityEngine.Debug.Break();
+        }
+        
+        [SuppressMessage("ReSharper", "Unity.PerformanceCriticalCodeInvocation")]
+        [Conditional("ENABLE_LOGS")]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void DebugWarning(string message)
+        {
+            //ReSharper disable once Unity.PerformanceCriticalCodeInvocation
+            UnityEngine.Debug.LogWarning("<color=yellow>" + message + "</color>");
+        }
+    }
+}
